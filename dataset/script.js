893101ba2d080e4e8b09d9c6ea24119a86b985ce
miguel sanglard <miguelsanglard@gmail.com>
2024-02-24 22:34:48 -0300

    data-set

---
+++ dataset/script.js
@@ -0,0 +1,29 @@
+// // Adicione um atributo data-anime="show-down" e
+// // data-anime="show-right" a todos as section's
+// // com descricão dos animais.
+
+// // Utilizando estes atributos, adicione a classe
+// // show-down ou show-right a sua respectiva section
+// // assim que a mesma aparecer na tela (animacao tab)
+
+// const showright = document.querySelectorAll('[data-anime="show-right"]')
+// const showdown = document.querySelectorAll('[data-anime="show-down"]')
+
+
+// // showright.forEach((item) => {
+// //   if (item.classList == 'ativo') {
+// //     item.classList.add('show-down')
+// //     console.log('possui ativo')
+// //   } else {
+// //     console.log('não possui ativo')
+// //   }
+// // })
+
+
+// console.log(showdown)
+
+// // No CSS faça com que show-down anime de cima para baixo
+// // e show-right continue com a mesma animação da esquerda
+// // para a direita
+
+// // Substitua todas as classes js- por data atributes.
